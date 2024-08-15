d0dbd7b364f9bd5c19f9933bcd037f75bab5fef3
Joe Snee <snoey.92@gmail.com>
2024-08-15 14:11:34 -0500

    Setup euler cube

---
+++ scripts/quaternion_demo.gd
@@ -0,0 +1,21 @@
+class_name QuaternionDemo extends Node3D
+
+@onready var x_axis_value: Label = $CanvasLayer/PanelContainer/MarginContainer/GridContainer/XAxisValue
+@onready var gimbal_x: MeshInstance3D = $GimbalX
+@onready var gimbal_z: MeshInstance3D = $GimbalX/GimbalZ
+@onready var gimbal_y: MeshInstance3D = $GimbalX/GimbalZ/GimbalY
+@onready var y_axis_value: Label = $CanvasLayer/PanelContainer/MarginContainer/GridContainer/YAxisValue
+@onready var z_axis_value: Label = $CanvasLayer/PanelContainer/MarginContainer/GridContainer/ZAxisValue
+
+
+func _on_y_axis_slider_value_changed(value: float) -> void:
+	gimbal_y.rotation_degrees.y = value
+	y_axis_value.text = str(value)
+
+func _on_x_axis_slider_value_changed(value: float) -> void:
+	gimbal_x.rotation_degrees.x = value
+	x_axis_value.text = str(value)
+
+func _on_z_axis_slider_value_changed(value: float) -> void:
+	gimbal_z.rotation_degrees.z = value + 90
+	z_axis_value.text = str(value)
