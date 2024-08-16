ca3438574f7477507c7f84ebc66d3a93cbea396c
snoeyz <108549238+snoeyz@users.noreply.github.com>
2024-08-15 23:35:07 -0500

    Fixed Euler demo

--- scripts/quaternion_demo.gd
+++ scripts/quaternion_demo.gd
@@ -8,13 +8,13 @@ class_name QuaternionDemo extends Node3D
 @onready var z_axis_value: Label = $CanvasLayer/PanelContainer/MarginContainer/GridContainer/ZAxisValue
 
 
-func _on_y_axis_slider_value_changed(value: float) -> void:
-	gimbal_y.rotation_degrees.y = value
-	y_axis_value.text = str(value)
-
 func _on_x_axis_slider_value_changed(value: float) -> void:
 	gimbal_x.rotation_degrees.x = value
 	x_axis_value.text = str(value)
+
+func _on_y_axis_slider_value_changed(value: float) -> void:
+	gimbal_y.rotation_degrees.x = value + 90
+	y_axis_value.text = str(value)
 
 func _on_z_axis_slider_value_changed(value: float) -> void:
 	gimbal_z.rotation_degrees.z = value + 90
